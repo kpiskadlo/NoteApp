98e1fffe8bdf0ebd01845690f9464dcfd2178b5d
Karolina Piskadło <k.piskadlo@intive.com>
2021-12-15 14:57:37 +0100

    feat: add swagger plugin

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,11 @@
 import { fastify } from 'fastify';
+import cors from 'fastify-cors';
+import swagger from './plugins/swagger';
 import { note } from './routes/note';
 
-export function create (options) {
-  return fastify(options).register(note, { prefix: 'note' });
+export function create(options) {
+  return fastify(options)
+    .register(cors, '*')
+    .register(swagger)
+    .register(note, { prefix: 'note' });
 }
---
+++ server/src/plugins/swagger.js
@@ -0,0 +1,17 @@
+import fp from 'fastify-plugin';
+import fastifySwagger from 'fastify-swagger';
+
+async function swaggerPlugin(fastify, options) {
+  return fastify.register(fastifySwagger, {
+    routePrefix: '/api',
+    exposeRoute: true,
+    openapi: {
+      info: {
+        title: 'Note App API',
+        version: '1.0'
+      }
+    }
+  });
+}
+
+export default fp(swaggerPlugin);
