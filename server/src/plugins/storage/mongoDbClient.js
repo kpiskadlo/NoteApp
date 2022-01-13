e55276a00b524d6729bc8f244ebadc0aafe31770
Karolina Piskadło <k.piskadlo@intive.com>
2022-01-13 13:29:29 +0100

    refactor: add todo comments

--- server/src/plugins/storage/mongoDbClient.js
+++ server/src/plugins/storage/mongoDbClient.js
@@ -4,10 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export function createMongoDbClient () {
-  return mongoose
-    .connect(process.env.MONGO_DB_CONNECTION_STRING)
-    .then(connection => {
-      return connection
-    })
-    .catch(error => console.error(error))
+  return (
+    mongoose
+      .connect(process.env.MONGO_DB_CONNECTION_STRING)
+      .then(connection => {
+        return connection
+      })
+      // TODO: Add logger
+      .catch(error => console.error(error))
+  )
 }
