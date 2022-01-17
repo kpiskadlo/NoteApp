4209a4201880d7dc2e6423b9e1030df6b9d8eac3
Karolina Piskadło <k.piskadlo@intive.com>
2022-01-17 11:01:41 +0100

    feat: use pino logger instead of console.log

--- server/src/plugins/storage/index.js
+++ server/src/plugins/storage/index.js
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin'
 import { createMongoDbClient } from './mongoDbClient'
 
 const storagePlugin = async (fastify, options) => {
-  await createMongoDbClient()
+  await createMongoDbClient(fastify)
 }
 
 export default fp(storagePlugin)
