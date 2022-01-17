5035d773a5f51d0dbabb885ec64bd0a5342af862
Karolina Piskadło <k.piskadlo@intive.com>
2022-01-17 10:59:34 +0100

    feat: use pino-pretty formatter

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,10 @@ import { create } from './app'
 
 const port = process.env.PORT ?? '3000'
 
-console.log(`\nListening on http://localhost:${port}\n`)
-
 create({
-  logger: true
+  logger: {
+    prettyPrint: true
+  }
 })
   .listen(port, '0.0.0.0')
   .catch(onError)
